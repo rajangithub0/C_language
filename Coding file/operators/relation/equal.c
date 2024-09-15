ea7dbedbc24b23d11e85c55653dc7abc022b47d1
Rajan <rajan.rana6@gmail.com>
2024-09-15 12:01:37 +0530

    arithmetic and relation operator

---
+++ Coding file/operators/relation/equal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    printf("%d \n", 4 == 4);//this condtion checked only datatype and value is ture then output will be 1.
+    printf("%d", 4 == 3); //this condtion is false then output is 0.
+    return 0;
+}
---
+++ Coding file/operators/relation/greaterthen.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    printf("%d \n", 4 > 3); //this condition is ture because 4 is bigger then 3 this program will output is 1.
+    printf("%d \n", 3 > 3);//this program is output is 0.
+    printf("%d \n", 3 >= 3); // this program condition is ture because 3 is not greaterthen 3 but this is equal according to this program output is ture. output 1.
+    return 0;
+}
